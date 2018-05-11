8fc3c97b215bfb147a5a9439c157f6f56b8cf296
sbx_user1059 <sbx_user1059@ip-10-12-29-234.eu-west-2.compute.internal>
2018-05-11 12:06:59 +0000

    Fri May 11 12:06:59 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,15 +1,33 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
+	
+//	private static final Map<Character, Integer> prices = new 
 	
     public Integer checkout(String skus) {
     	
-    	int total = 0;
-        char[] chars = skus.toCharArray();
+        Map<Character, Integer> counts = new HashMap<Character, Integer>();
+    	char[] chars = skus.toCharArray();
         for (char c : chars) {
-			
+			Integer count = counts.get(c);
+			if (count == null) {
+				count = 1;
+			} else {
+				++count;
+			}
+			counts.put(c, count);
 		}
         
+        for (Character c : counts.keySet()) {
+        	int count = counts.get(c); 
+			
+        	
+		}
+        
+    	int total = 0;
         return total;
     }
 }
