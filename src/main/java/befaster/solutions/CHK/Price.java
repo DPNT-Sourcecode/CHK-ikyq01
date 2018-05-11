a1e38885eb9913efacf5d173d123826d0b5a0a72
sbx_user1059 <sbx_user1059@ip-10-12-29-234.eu-west-2.compute.internal>
2018-05-11 13:02:51 +0000

    Fri May 11 13:02:51 UTC 2018

--- src/main/java/befaster/solutions/CHK/Price.java
+++ src/main/java/befaster/solutions/CHK/Price.java
@@ -1,5 +1,8 @@
 package befaster.solutions.CHK;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * TODO Comments!
  * 
@@ -9,30 +12,22 @@ public class Price {
 	
 	private final int basePrice;
 	
-	private final Integer offerCount;
-	// If the offer count is reached, either the offerPrice is charged, or a unit with the given offer code is given free
-	// (current implementation assumes only a single item will be given free)
-	private final Integer offerPrice;
+	private final List<MultiBuy> multiBuys = new ArrayList<MultiBuy>();
 	private final Character offerCode;
 	
 	public Price(int basePrice) {
 		this.basePrice = basePrice;
-		this.offerCount = null;
-		this.offerPrice = null;
 		this.offerCode = null;
 	}
 	
 	public Price(int basePrice, Integer offerCount, Integer offerPrice) {
 		this.basePrice = basePrice;
-		this.offerCount = offerCount;
-		this.offerPrice = offerPrice;
+		multiBuys.add(new MultiBuy(offerCount, offerPrice));
 		this.offerCode = null;
 	}
 	
 	public Price(int basePrice, Integer offerCount, Character offerCode) {
 		this.basePrice = basePrice;
-		this.offerCount = offerCount;
-		this.offerPrice = null;
 		this.offerCode = offerCode;
 	}
 	
