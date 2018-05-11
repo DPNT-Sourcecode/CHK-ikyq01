75a7caa02793f61d310b5adab0ca5fb3d75d5e49
sbx_user1059 <sbx_user1059@ip-10-12-29-234.eu-west-2.compute.internal>
2018-05-11 14:01:21 +0000

    Fri May 11 14:01:21 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,26 +8,6 @@ import java.util.Map;
 
 public class CheckoutSolution {
 	
-	private static final Map<Character, Price> prices = new HashMap<Character, Price>();
-	static {
-		List<BulkBuyOffer> bulkBuyOffers = new ArrayList<BulkBuyOffer>();
-		bulkBuyOffers.add(new BulkBuyOffer(3, 130));
-		bulkBuyOffers.add(new BulkBuyOffer(5, 200));
-		prices.put('A', new Price(50, bulkBuyOffers));
-		
-		prices.put('B', new Price(30, 2, 45));
-		prices.put('C', new Price(20));
-		prices.put('D', new Price(15));
-		prices.put('E', new Price(40, 2, 'B'));
-
-		// Implement initially using a BulkBuyOffer
-//		Our marketing team wants to try rewording the offer to see if it affects consumption
-//		Instead of multi-pricing this item they want to say "buy 2Fs and get another F free"
-//		The offer requires you to have 3 Fs in the basket.
-		// TODO Is there any practical difference? 
-		prices.put('F', new Price(10, 3, 20));
-	}
-
 //	Our price table and offers: 
 //		+------+-------+----------------+
 //		| Item | Price | Special offers |
