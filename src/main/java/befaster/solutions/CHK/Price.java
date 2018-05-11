ea8ef800035f5b49cf14535e0495eef64455c860
sbx_user1059 <sbx_user1059@ip-10-12-29-234.eu-west-2.compute.internal>
2018-05-11 14:56:55 +0000

    Fri May 11 14:56:55 UTC 2018

--- src/main/java/befaster/solutions/CHK/Price.java
+++ src/main/java/befaster/solutions/CHK/Price.java
@@ -15,24 +15,34 @@ public class Price implements Comparable<Price> {
 	
 	private final List<BulkBuyOffer> bulkBuyOffers = new ArrayList<BulkBuyOffer>();
 	private final List<GetItemsFreeOffer> getItemsFreeOffers = new ArrayList<GetItemsFreeOffer>();
+	private final GroupDiscountOffer groupDiscountOffer;
 	
 	public Price(int basePrice) {
 		this.basePrice = basePrice;
+		groupDiscountOffer = null;
 	}
 	
 	public Price(int basePrice, int offerCount, int offerPrice) {
 		this.basePrice = basePrice;
 		bulkBuyOffers.add(new BulkBuyOffer(offerCount, offerPrice));
+		groupDiscountOffer = null;
 	}
 	
 	public Price(int basePrice, int offerCount, char offerCode) {
 		this.basePrice = basePrice;
 		getItemsFreeOffers.add(new GetItemsFreeOffer(offerCount, offerCode));
+		groupDiscountOffer = null;
 	}
 	
 	public Price(int basePrice, List<BulkBuyOffer> bulkBuyOffers) {
 		this.basePrice = basePrice;
 		this.bulkBuyOffers.addAll(bulkBuyOffers);
+		groupDiscountOffer = null;
+	}
+	
+	public Price(int basePrice, GroupDiscountOffer groupDiscountOffer) {
+		this.basePrice = basePrice;
+		groupDiscountOffer = groupDiscountOffer;
 	}
 	
 	public char getCode() {
