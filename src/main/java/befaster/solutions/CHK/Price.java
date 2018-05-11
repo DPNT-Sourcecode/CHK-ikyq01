c0b9259ff4edfe77807a9e80354075cc92ddca16
sbx_user1059 <sbx_user1059@ip-10-12-29-234.eu-west-2.compute.internal>
2018-05-11 12:09:54 +0000

    Fri May 11 12:09:54 UTC 2018

---
+++ src/main/java/befaster/solutions/CHK/Price.java
@@ -0,0 +1,33 @@
+package befaster.solutions.CHK;
+
+public class Price {
+	
+	private final int basePrice;
+	private final Integer offerCount;
+	private final Integer offerPrice;
+	
+	public Price(int basePrice) {
+		this.basePrice = basePrice;
+		this.offerCount = null;
+		this.offerPrice = null;
+	}
+	
+	public Price(int basePrice, Integer offerCount, Integer offerPrice) {
+		this.basePrice = basePrice;
+		this.offerCount = offerCount;
+		this.offerPrice = offerPrice;
+	}
+
+	public int getBasePrice() {
+		return basePrice;
+	}
+
+	public Integer getOfferCount() {
+		return offerCount;
+	}
+
+	public Integer getOfferPrice() {
+		return offerPrice;
+	}
+	
+}
