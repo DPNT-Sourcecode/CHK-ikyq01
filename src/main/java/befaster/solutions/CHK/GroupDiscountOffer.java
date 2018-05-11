15ec3319a29e01848b35457bb729d53865eae073
sbx_user1059 <sbx_user1059@ip-10-12-29-234.eu-west-2.compute.internal>
2018-05-11 14:26:56 +0000

    Fri May 11 14:26:56 UTC 2018

--- src/main/java/befaster/solutions/CHK/GroupDiscountOffer.java
+++ src/main/java/befaster/solutions/CHK/GroupDiscountOffer.java
@@ -1,5 +1,32 @@
 package befaster.solutions.CHK;
 
+import java.util.Set;
+
 public class GroupDiscountOffer {
+
+	private final Set<Character> codes;
+	private final int offerPrice;
+	
+	public GroupDiscountOffer(Set<Character> codes, int offerPrice) {
+		this.codes = codes;
+		this.offerPrice = offerPrice;
+	}
+	
+	public Set<Character> getCodes() {
+		return codes;
+	}
+
+	public Integer getOfferPrice() {
+		return offerPrice;
+	}
+	
+	public boolean equals(Object object) {
+		if (object instanceof GroupDiscountOffer) {
+			GroupDiscountOffer offer = (GroupDiscountOffer) object;
+			// TODO null handling
+			return offer.codes.equals(this.codes) && offer.offerPrice == this.offerPrice;
+		}
+		return false;
+	}
 
 }
