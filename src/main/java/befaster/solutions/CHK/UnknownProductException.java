756bd95ea91b4a85a17c3d162002f2563b11ed0c
Name <Name@LAPTOP-FHFH6G73.localdomain>
2018-05-20 13:14:21 +0100

    New

---
+++ src/main/java/befaster/solutions/CHK/UnknownProductException.java
@@ -0,0 +1,10 @@
+package befaster.solutions.CHK;
+
+@SuppressWarnings("serial")
+public class UnknownProductException extends Exception {
+	
+	public UnknownProductException(String message) {
+		super(message);
+	}
+
+}
