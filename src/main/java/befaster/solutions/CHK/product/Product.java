1b1449d776455fbdf3907822dc6f07c548f327d5
Name <Name@LAPTOP-FHFH6G73>
2018-05-23 11:47:22 +0100

    Refactored constructors

--- src/main/java/befaster/solutions/CHK/product/Product.java
+++ src/main/java/befaster/solutions/CHK/product/Product.java
@@ -9,44 +9,51 @@ import befaster.solutions.CHK.offer.GroupDiscountOffer;
 
 /**
  * A product, containing a code, base price, and any applicable offers.
- * 
- * TODO Refactor constructors
  */
 public class Product implements Comparable<Product> {
 	
 	private char code;
 	private final int basePrice;
 	
+	// TODO? A more generic solution is possible, but would the extra complexity be justified?
+	//       (e.g one might use a map from offer codes to a list of offers)
 	private final List<GetProductsFreeOffer> getProductsFreeOffers = new ArrayList<>();
 	private final List<BulkBuyOffer> bulkBuyOffers = new ArrayList<>();
 	private final GroupDiscountOffer groupDiscountOffer;
 	
-	public Product(int basePrice) {
+	public Product(
+			int basePrice,
+			List<GetProductsFreeOffer> getProductsFreeOffers,
+			List<BulkBuyOffer> bulkBuyOffers,
+			GroupDiscountOffer groupDiscountOffer) {
+		
 		this.basePrice = basePrice;
-		this.groupDiscountOffer = null;
-	}
-	
-	public Product(int basePrice, int offerCount, int offerPrice) {
-		this.basePrice = basePrice;
-		this.bulkBuyOffers.add(new BulkBuyOffer(offerCount, offerPrice));
-		this.groupDiscountOffer = null;
-	}
-	
-	public Product(int basePrice, int numberOfChargedProducts, char freeProductCode) {
-		this.basePrice = basePrice;
-		this.getProductsFreeOffers.add(new GetProductsFreeOffer(numberOfChargedProducts, 1, freeProductCode));
-		this.groupDiscountOffer = null;
-	}
-	
-	public Product(int basePrice, List<BulkBuyOffer> bulkBuyOffers) {
-		this.basePrice = basePrice;
-		this.bulkBuyOffers.addAll(bulkBuyOffers);
-		this.groupDiscountOffer = null;
-	}
-	
-	public Product(int basePrice, GroupDiscountOffer groupDiscountOffer) {
-		this.basePrice = basePrice;
+		if (getProductsFreeOffers != null) {
+			this.getProductsFreeOffers.addAll(getProductsFreeOffers);
+		}
+		if (bulkBuyOffers != null) {
+			this.bulkBuyOffers.addAll(bulkBuyOffers);
+		}
 		this.groupDiscountOffer = groupDiscountOffer;
+	}
+	
+	// Convenience constructors for coding challenge implementation of ProductDaoImpl
+	protected Product(int basePrice) {
+		this(basePrice, null, null, null);
+	}
+	protected Product(int basePrice, int offerCount, int offerPrice) {
+		this(basePrice, null, null, null);
+		this.bulkBuyOffers.add(new BulkBuyOffer(offerCount, offerPrice));
+	}
+	protected Product(int basePrice, int numberOfChargedProducts, char freeProductCode) {
+		this(basePrice, null, null, null);
+		this.getProductsFreeOffers.add(new GetProductsFreeOffer(numberOfChargedProducts, 1, freeProductCode));
+	}
+	protected Product(int basePrice, List<BulkBuyOffer> bulkBuyOffers) {
+		this(basePrice, null, bulkBuyOffers, null);
+	}
+	protected Product(int basePrice, GroupDiscountOffer groupDiscountOffer) {
+		this(basePrice, null, null, groupDiscountOffer);
 	}
 	
 	public char getCode() {
